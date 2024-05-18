cfeafbfa35ab3ffa44bba19d72e3a2ba2bedafc6
mariiishka <79147126+mariiishka@users.noreply.github.com>
2024-05-18 03:04:54 +0300

    fixed pdf worker issue

--- src/entities/pdf-viewer/ui/pdf-viewer.tsx
+++ src/entities/pdf-viewer/ui/pdf-viewer.tsx
@@ -1,15 +1,17 @@
 'use client';
 
 import React from 'react';
-import {Document, type DocumentProps} from 'react-pdf';
-import {pdfjs} from 'react-pdf';
+import {Document, type DocumentProps, pdfjs} from 'react-pdf';
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
 import {cn} from '@/shared/ui/utils';
 
+const options = {workerSrc: '/pdf.worker.js'};
+
 export function PDFViewer({children, className, ...props}: DocumentProps) {
   return (
     <Document
+      options={options}
       className={cn('w-fit', className)}
       // TODO: error & loading & noData components
       error={<div className="hidden" />}
